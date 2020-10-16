caecfa63dee1373feec3e9b8fed1e532a727285c
Ryan <57524540+superioran-deviap@users.noreply.github.com>
2020-10-16 11:03:10 -0700

    updated embeds

--- commands/update.js
+++ commands/update.js
@@ -1,22 +1,33 @@
 module.exports = {
     name: 'update',
     aliases: ['upd', 'new'],
-    description: 'Update',
+    description: 'Update channels',
     guildOnly: true,
 	execute(client, message, args) {
-        const { deviapInfo }= require('../commands/response.js')
-        const infoChannel = client.channels.get('766712471230611527')
+        const { serverInfo, serverSupport, serverRules }= require('../commands/response.js')
+        const infoChannel = client.channels.cache.get('766712471230611527')
+        const supportChannel = client.channels.cache.get('760592899351379998')
+        const ruleChannel = client.channels.cache.get('760593027734569050')
         if(args.length > 1){
             message.delete()
             message.reply('Too many arguments!')
         }
-        else if(args.length < 1){
+        else if(!args.length){
             message.delete()
             message.reply('Not enough arguments!')
         }
         else if(args[0] === 'info'){
             message.delete()
-            infoChannel.send({embed: deviapInfo(client)})
+            infoChannel.bulkDelete(3).then(
+            infoChannel.send({embed: serverInfo(client)}))
+        }
+        else if(args[0] === 'support'){
+            message.delete()
+            supportChannel.send({embed: serverSupport(client)})
+        }
+        else if(args[0] === 'rules'){
+            message.delete()
+            ruleChannel.send({embed: serverRules(client)})
         }
     }
 }
