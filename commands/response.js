c46bce92953733d766e6f8b128475de3f8407232
Ryan <57524540+superioran-deviap@users.noreply.github.com>
2020-10-16 11:02:34 -0700

    updated responses

--- commands/response.js
+++ commands/response.js
@@ -39,3 +39,29 @@ module.exports.serverInfo =  (client) => {
         .setTimestamp();
         return deviapInfo;
 }
+
+// Support
+module.exports.serverSupport = (client) => {
+    const supportInfo = new MessageEmbed()
+        .setColor('#6767ce')
+        .setTitle('Support and Assistance')
+        .setDescription('Hello! 👋\n\nTo help make it easier to receive a response to your support ticket, we only allow you to ask for support through the official Deviap support email, listed below. We offer support through your individual ticket so that we can clearly communicate with you. Use the ticket to ask any questions, offer us great ideas about the platform, or let us know if you are experiencing an issue with our product.')
+        .addField('I have an open ticket, but I did not get a response!', 'Please ensure that your mailbox is not marking Deviap as Spam. Additionally, check your Junk or Spam folder!')
+        .addField('Contact us!', '**support@deviap.com**')
+        .setTimestamp();
+        return supportInfo;
+}
+// Rules
+module.exports.serverRules = (client) => {
+    const deviapRules = new MessageEmbed()
+        .setColor('#6767ce')
+        .setTitle('Community Rules')
+        .setDescription('The following rules are not exhaustive of all scenarios. Use common sense when interacting with others.')
+        .addField('**1. No Hate Speech**', 'Hate speech in any form is not tolerated, whether spoken in a Voice channel or sent as "User Generated Content" (messages or custom statuses). ')
+        .addField('**2. No Harassment**', 'Do not harass any member of the community for any reason. No means no.')
+        .addField('**3. No Advertising**', 'Do not advertise anything or solicit invitations to members through UGC (messages sent in DMs or here, custom statuses, or interactions in a Voice Channel).')
+        .addField('**4. No Inappropriate Content**', 'Inappropriate content (NSFW/L content) is not allowed at all in Deviap, including UGC (as defined previously).')
+        .addField('**5. No Spam / Copypasta**', 'Spamming messages, flooding channels, or sending chainmail is not allowed.')
+        .setTimestamp();
+        return deviapRules;
+}
